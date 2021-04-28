45d55d2367dddc4e391b44781ab2c82db1645e4a
Lubica Daubnerova <83361384+LubicaDaubnerova@users.noreply.github.com>
2021-04-28 18:13:33 +0200

    Add files via upload

---
+++ Madlib Eng.py
@@ -0,0 +1,21 @@
+noun1 = input("Noun plural: ")
+noun2 = input("Noun plural: ")
+adjective1 = input("Adjective: ")
+adjective2 = input("Adjective: ")
+
+madlib = f"""In some parts of the world where {noun1} used to fall
+regularly, there is now less {noun1}, or no {noun1} at all. In other
+places where there used to be a few bad {noun2} every year, there
+are now lots of {noun2}, and they are very {adjective1}.
+Across many parts of the world there are also many more {adjective2}
+floods than there used to be."""
+
+print(madlib)
+
+#Abigail Wheatley: The story of planet Earth
+#In some parts of the world where rain used to fall
+#regularly, there is now less rain, or no rain at all. In other
+#places where there used to be a few bad storm every year, there
+#are now lots of storms, and they are very bad.
+#Across many parts of the world there are also many more devastating
+#floods than there used to be.
