2c4920bdf2cfeb3a0a5d1dbfbaaa7b7648af622f
Lubica Daubnerova <83361384+LubicaDaubnerova@users.noreply.github.com>
2021-04-28 18:15:04 +0200

    Add files via upload

---
+++ Madlib Sk.py
@@ -0,0 +1,28 @@
+podst_m1 = input("podstatné m.,sing.,L: ")
+podst_m2 = input("podstatné m.,ŽR,sing.,L: ")
+sloveso1 = input("sloveso, min.č., pl., 1.os.: ")
+podst_m3 = input("podstatné m.,ŽR,sing.,N: ")
+podst_m4 = input("podstatné m.,MR,sing.,N: ")
+podst_m5 = input("podstatné m.,A: ")
+sloveso2 = input("sloveso, min.č., pl., 1.os.: ")
+sloveso3 = input("sloveso, min.č., pl., 1.os.: ")
+
+madlib = f"Vydržať celý deň na {podst_m1} bola nuda. V cudzej {podst_m2} nám \
+nikto neotvoril malú telocvičňu, a keď sme {sloveso1} po žinenkách \
+naskladaných na chodbe, nejaká {podst_m3} na nás nakričala a zakázala nám \
+to. Do školy sme nevošli, lebo všetko bolo pozamykané. {podst_m4} nám \
+nakoniec odniekiaľ vydrankal {podst_m5} a pustil nás so Števom na \
+školský dvor. Chvíľu sme sa so Števom {sloveso2}, ale potom k nám \
+{sloveso3} nejakí chalani."
+
+print(madlib)
+
+#PôVODNÁ VERZIA
+#Gabriela Futová: Čo učky nevidia.
+#Vydržať celý deň na turnaji bola nuda. V cudzej škole nám
+#nikto neotvoril malú telocvičňu, a keď sme skákali po žinenkách
+#naskladaných na chodbe, nejaká učka na nás nakričala a zakázala nám
+#to. Do školy sme nevošli, lebo všetko bolo pozamykané. Oco nám
+#nakoniec odniekiaľ vydrankal futbalku a pustil nás so Števom na
+#školský dvor. Chvíľu sme sa so Števom naháňali, ale potom k nám
+#prišli nejakí chalani.
